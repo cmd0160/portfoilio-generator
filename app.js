af89e0a9772de8dbd8f5d80d24a1b5d3a87b558e
cdtest <corydavis1989@gmail.com>
2022-04-19 10:47:04 -0500

    fs and node core modules review

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
+// saves the core module to the fs variable
 const fs = require("fs");
-const generatePage = require("./src/page-template.js");
+const generatePage = require('./src/page-template')
 
-const profileDataArgs = process.argv.slice(2);
+const profileDataArgs = process.argv.slice(2, process.argv.length);
 
 const [name, github] = profileDataArgs;
 
-fs.writeFile("./index.html", generatePage(name, github), (err) => {
-  if (err) throw new Error(err);
+fs.writeFile("index.html", generatePage(name, github), (err) => {
+  if (err) throw err;
 
   console.log("Portfolio complete! Check out index.html to see the output!");
 });
